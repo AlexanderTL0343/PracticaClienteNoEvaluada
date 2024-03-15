d4b07fba0713f27d32c90b958c7afb941495d012
AlexanderTL0343 <145624722+AlexanderTL0343@users.noreply.github.com>
2024-03-14 19:51:32 -0600

    Add files via upload

---
+++ RedesSwing/RedesSwingCliente/src/main/java/com/mycompany/redesswingcliente/RedesSwingCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.redesswingcliente;
+
+/**
+ *
+ * @author alext
+ */
+public class RedesSwingCliente {
+
+    public static void main(String[] args) {
+       LoginSwing interfaz = new LoginSwing();
+       interfaz.activar();
+    }
+}
